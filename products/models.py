63e06f8b72e7adbb1ceef673f4126c1eb74e36c4
MathanVms <vmathanvmkumar@gmail.com>
2026-08-30 11:05:16 +0530

    Refactor models.py: Improve formatting and add __str__ method for Order model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -9,7 +10,8 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(
@@ -17,15 +19,71 @@ class Product(models.Model):
         on_delete=models.CASCADE
     )
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
     quantity = models.PositiveIntegerField()
     unit = models.CharField(max_length=20)
     harvest_date = models.DateField()
     image = models.ImageField(
-        upload_to='products/',
+        upload_to="products/",
         blank=True,
         null=True
     )
 
     def __str__(self):
         return self.name
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    full_name = models.CharField(max_length=100)
+
+    phone = models.CharField(max_length=15)
+
+    address = models.TextField()
+
+    city = models.CharField(max_length=50)
+
+    pincode = models.CharField(max_length=10)
+
+    payment_method = models.CharField(
+        max_length=20,
+        default="Cash on Delivery"
+    )
+
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.user.username}"
